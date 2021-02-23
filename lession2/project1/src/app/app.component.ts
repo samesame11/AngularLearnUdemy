5c6db2ea4ca658e30b68fe67c1106051f03bf9be
samesame11 <47394405+samesame11@users.noreply.github.com>
2021-02-23 14:20:22 +0700

    update

--- lession2/project1/src/app/app.component.ts
+++ lession2/project1/src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-  constructor() {
-    
-  }
-
-  onToggleDetails() {
-    this.showSecret =! this.showSecret
-    this.log.push(this.log.length + 1)
-  }
+  title = 'project1';
 }
